9c8c063014e5d4d34f767089a7c8e2daa937a8e5
smntgl <simone.tagliente@mail.polimi.it>
2021-08-03 12:20:50 +0200

    setup

--- SourceCode.c
+++ SourceCode.c
@@ -2,6 +2,12 @@
 // Created by Simone Tagliente on 02/08/2021.
 //
 #include<stdio.h>
+#include<stdlib.h>
+
+typedef struct graph {
+    int index;
+    struct graph * next;
+};
 
 int main(){
     
